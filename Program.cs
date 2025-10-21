fe909551b1e4a972a49e9037c8de4fef2035fde3
J-F Bilodeau <jfbilodeau@chronogears.com>
2025-10-21 13:05:21 -0400

    Added beautiful logo

--- Program.cs
+++ Program.cs
@@ -13,6 +13,10 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+var username = "dba";
+var password = "s3cr3t!";
+app.ConnectToDatabase(username, password);
+
 app.UseHttpsRedirection();
 
 app.UseRouting();
